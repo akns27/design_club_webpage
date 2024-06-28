b86bda568ae35ee815c33aab5ea00fb1b8938f48
KangwonPark <nambaggangwon@gmail.com>
2024-06-28 13:39:04 +0900

    메인페이지 작업 완료

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import "../style.css";
+
+function Footer() {
+  return (
+    <div className="footer_container">
+      <div className="footer_title left-text">busan software</div>
+      <div className="footer_title right-text">meister high school</div>
+      <div className="footer_title right-text">1f design room,</div>
+      <div className="footer_title left-text">1393, garak-daero,</div>
+      <div className="footer_title right-text">gangseo-gu.</div>
+      <div className="footer-copyright left-text">
+      ⓒbssm design club all rights reserved
+    </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Heropage.jsx
@@ -0,0 +1,17 @@
+import "../style.css";
+
+function Heropage() {
+  return (
+    <div className="hero_container">
+      <div className="hero_text">
+        <div>Our deSign</div>
+        <div>cLub is A clUb</div>
+        <div>tHat eVolves</div>
+        <div>thRough hOw </div>
+        <div style={{ whiteSpace: "pre-wrap" }}>caN Be ( )</div>
+      </div>
+    </div>
+  );
+}
+
+export default Heropage;
