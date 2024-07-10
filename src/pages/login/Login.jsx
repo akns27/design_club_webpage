b8cee0f3d6e06c39071b89a8a323fd19416b1dc2
이재현 <2023050@bssm.hs.kr>
2024-07-10 12:23:21 +0900

    ADD :: 로그인 페이지 완료

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,7 +1,31 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Input from '../../components/login&signup/input/Input';
+import Logo from '../../components/login&signup/logo/Logo';
+import Submit from '../../components/login&signup/submit/Submit';
+import './login.css'
+
 function Login(){
+    const navigator = useNavigate();
+
+    const goSignup = () => {
+        navigator('/signup');
+    }
+
     return (
-        <div></div>
+        <div id='login'>
+            <Logo />
+            <div className='frm'>
+                <p>로그인</p>
+                <div className="inputBox">
+                    <Input className="id" placeholder="아이디를 입력해주세요" />
+                    <Input className='pwd' placeholder="비밀번호를 입력해주세요" />
+                </div>
+                <Submit type="로그인" />
+                <div className='signup' onClick={goSignup}>회원가입</div>
+            </div>
+        </div>
     );
 }
 
-export default Login;
+export default  Login;
